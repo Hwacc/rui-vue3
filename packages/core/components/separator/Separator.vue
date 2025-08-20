541f0fa729a13f202147aebb4a67096ae7b81b9c
chuancheng.hua@razer.com <chuancheng.hua@razer.com>
2025-08-20 10:16:59 +0800

    fix: separator cva -> tv

--- packages/core/components/separator/Separator.vue
+++ packages/core/components/separator/Separator.vue
@@ -1,50 +1,45 @@
 <script setup lang="ts">
 import type { SeparatorProps } from 'reka-ui'
 import type { HTMLAttributes } from 'vue'
-import { cn } from '@rui/core/lib/utils'
 import { Separator } from 'reka-ui'
-import { separatorLabelVariants, separatorVariants } from '.'
+import { tvSeparator } from '.'
 
 const {
   class: propsClass,
-  labelClass,
   unstyled,
   label,
+  ui,
   ...props
 } = defineProps<
   SeparatorProps & {
     class?: HTMLAttributes['class']
-    labelClass?: HTMLAttributes['class']
     unstyled?: boolean
     label?: string
+    ui?: {
+      root?: {
+        class?: HTMLAttributes['class']
+      }
+      label?: {
+        class?: HTMLAttributes['class']
+      }
+    }
   }
 >()
+
+const { base, label: tvLabel } = tvSeparator({
+  orientation: props.orientation,
+  unstyled,
+})
 </script>
 
 <template>
   <Separator
     v-bind="props"
-    :class="
-      cn(
-        separatorVariants({
-          orientation: props.orientation,
-          unstyled,
-        }),
-        propsClass,
-      )
-    "
+    :class="base({ class: [ui?.root?.class, propsClass] })"
   >
     <span
       v-if="label"
-      :class="
-        cn(
-          separatorLabelVariants({
-            orientation: props.orientation,
-            unstyled,
-          }),
-          labelClass,
-        )
-      "
+      :class="tvLabel({ class: ui?.label?.class })"
     >
       {{ label }}
     </span>
--- packages/core/components/separator/index.ts
+++ packages/core/components/separator/index.ts
@@ -1,46 +1,52 @@
-import type { VariantProps } from '@rui/core/lib/cva'
+import type { VariantProps } from 'tailwind-variants'
 import { PREFIX } from '@rui/core/lib/constants'
-import { cva } from '@rui/core/lib/cva'
+import { tv } from '@rui/core/lib/tv'
 
 export { default as Separator } from './Separator.vue'
 
-export const separatorVariants = cva(
-  ['shrink-0 bg-border relative'],
+export const tvSeparator = tv(
   {
+    base: ['shrink-0 bg-border relative'],
+    slots: {
+      label: [
+        'text-xs',
+        'absolute',
+        'top-1/2',
+        'left-1/2',
+        '-translate-x-1/2',
+        '-translate-y-1/2',
+        'flex',
+        'justify-center',
+        'items-center',
+      ],
+    },
     variants: {
       orientation: {
         vertical: ['w-px h-full'],
         horizontal: ['h-px w-full'],
       },
     },
-  },
-  {
-    className: `${PREFIX}-separator`,
-  },
-)
-export type SeparatorProps = VariantProps<typeof separatorVariants>
-
-export const separatorLabelVariants = cva(
-  [
-    'text-xs',
-    'absolute',
-    'top-1/2',
-    'left-1/2',
-    '-translate-x-1/2',
-    '-translate-y-1/2',
-    'flex',
-    'justify-center',
-    'items-center',
-  ],
-  {
-    variants: {
-      orientation: {
-        vertical: ['w-px px-1 py-2'],
-        horizontal: ['h-px py-1 px-2'],
+    defaultVariants: {
+      orientation: 'horizontal',
+    },
+    compoundSlots: [
+      {
+        slots: ['label'],
+        orientation: 'vertical',
+        class: ['w-px px-1 py-2'],
       },
+      {
+        slots: ['label'],
+        orientation: 'horizontal',
+        class: ['h-px py-1 px-2'],
+      },
+    ],
+  },
+  {
+    class: `${PREFIX}-separator`,
+    slots: {
+      label: `${PREFIX}-separator-label`,
     },
   },
-  {
-    className: `${PREFIX}-separator-label`,
-  },
 )
+export type SeparatorVariants = VariantProps<typeof tvSeparator>
