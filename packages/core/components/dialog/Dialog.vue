b29c5e595660c9e3323b15ccdddc4a08a849ecc1
chuancheng.hua@razer.com <chuancheng.hua@razer.com>
2025-09-22 15:16:46 +0800

    feat: add dialog close preventDefault

--- packages/core/components/dialog/Dialog.vue
+++ packages/core/components/dialog/Dialog.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import type { DialogRootEmits, DialogRootProps } from 'reka-ui'
-import type { HTMLAttributes } from 'vue'
-import { DialogRoot, useForwardPropsEmits } from 'reka-ui'
-import { tvDialog } from '.'
-import DialogProvider from './DialogRootProviderEx'
+import type { DialogRootEmits, DialogRootProps } from 'reka-ui';
+import type { HTMLAttributes } from 'vue';
+import { DialogRoot, useForwardPropsEmits } from 'reka-ui';
+import { tvDialog } from '.';
+import DialogProvider from './DialogRootProviderEx';
 
 const {
   class: propsClass,
   unstyled,
   ...props
-} = defineProps<DialogRootProps & { class?: HTMLAttributes['class'], unstyled?: boolean }>()
-const emits = defineEmits<DialogRootEmits>()
+} = defineProps<DialogRootProps & { class?: HTMLAttributes['class']; unstyled?: boolean }>();
+const emits = defineEmits<DialogRootEmits>();
 
-const { root } = tvDialog()
-const forwarded = useForwardPropsEmits(props, emits)
+const { root } = tvDialog();
+const forwarded = useForwardPropsEmits(props, emits);
 </script>
 
 <template>
--- packages/core/components/dialog/DialogClose.vue
+++ packages/core/components/dialog/DialogClose.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import type { DialogCloseProps } from 'reka-ui'
-import type { HTMLAttributes } from 'vue'
-import type { DialogCloseFrom } from '.'
-import { DialogClose, useForwardProps } from 'reka-ui'
-import { tvDialog } from '.'
-import { injectDialogContext } from './DialogRootProviderEx'
+import type { DialogCloseProps, PrimitiveProps } from 'reka-ui';
+import type { HTMLAttributes } from 'vue';
+import type { DialogCloseFrom } from '.';
+import { Primitive, useForwardProps } from 'reka-ui';
+import { tvDialog } from '.';
+import { injectDialogContext } from './DialogRootProviderEx';
 
 const {
   as = 'div',
@@ -13,30 +13,45 @@ const {
   unstyled,
   ...props
 } = defineProps<
-  DialogCloseProps & {
-    as?: DialogCloseProps['as']
-    class?: HTMLAttributes['class']
-    closeFrom?: DialogCloseFrom
-    unstyled?: boolean
+  PrimitiveProps & {
+    as?: DialogCloseProps['as'];
+    class?: HTMLAttributes['class'];
+    closeFrom?: DialogCloseFrom;
+    unstyled?: boolean;
   }
->()
-const { closeFrom: contextCloseFrom } = injectDialogContext()
+>();
 
-function onClose() {
-  contextCloseFrom.value = closeFrom
+const emits = defineEmits<{
+  click: [e: Event];
+  close: [from?: DialogCloseFrom];
+}>();
+
+const { onOpenChange, closeFrom: contextCloseFrom } = injectDialogContext();
+
+function handleClose(from?: DialogCloseFrom) {
+  contextCloseFrom.value = from ?? closeFrom;
+  emits('close', contextCloseFrom.value);
+  onOpenChange(false);
 }
 
-const { close } = tvDialog()
-const forwarded = useForwardProps(props)
+function onClose(event: Event, from?: DialogCloseFrom) {
+  emits('click', event);
+  console.log('click', event.defaultPrevented);
+  if (event.defaultPrevented) return true;
+  handleClose(from);
+}
+
+const { close } = tvDialog();
+const forwarded = useForwardProps(props);
 </script>
 
 <template>
-  <DialogClose
+  <Primitive
     v-bind="forwarded"
     :as="as"
-    :class="close({ unstyled })"
-    @click="onClose"
+    :class="close({ unstyled, class: propsClass })"
+    @click="(e) => onClose(e, closeFrom)"
   >
-    <slot />
-  </DialogClose>
+    <slot v-bind="{ close: handleClose }" />
+  </Primitive>
 </template>
