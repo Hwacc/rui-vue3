2f0e60ff12dacfcb969e726a99355ab77cd227d7
chuancheng.hua@razer.com <chuancheng.hua@razer.com>
2025-09-23 15:51:47 +0800

    fix: format code

--- packages/core/components/dialog/DialogClose.vue
+++ packages/core/components/dialog/DialogClose.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-import type { DialogCloseProps, PrimitiveProps } from 'reka-ui';
-import type { HTMLAttributes } from 'vue';
-import type { DialogCloseFrom } from '.';
-import { Primitive, useForwardProps } from 'reka-ui';
-import { tvDialog } from '.';
-import { injectDialogContext } from './DialogRootProviderEx';
+import type { DialogCloseProps, PrimitiveProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import type { DialogCloseFrom } from '.'
+import { Primitive, useForwardProps } from 'reka-ui'
+import { tvDialog } from '.'
+import { injectDialogContext } from './DialogRootProviderEx'
 
 const {
   as = 'div',
@@ -14,35 +14,36 @@ const {
   ...props
 } = defineProps<
   PrimitiveProps & {
-    as?: DialogCloseProps['as'];
-    class?: HTMLAttributes['class'];
-    closeFrom?: DialogCloseFrom;
-    unstyled?: boolean;
+    as?: DialogCloseProps['as']
+    class?: HTMLAttributes['class']
+    closeFrom?: DialogCloseFrom
+    unstyled?: boolean
   }
->();
+>()
 
 const emits = defineEmits<{
-  click: [e: Event];
-  close: [from?: DialogCloseFrom];
-}>();
+  click: [e: Event]
+  close: [from?: DialogCloseFrom]
+}>()
 
-const { onOpenChange, closeFrom: contextCloseFrom } = injectDialogContext();
+const { onOpenChange, closeFrom: contextCloseFrom } = injectDialogContext()
 
 function handleClose(from?: DialogCloseFrom) {
-  contextCloseFrom.value = from ?? closeFrom;
-  emits('close', contextCloseFrom.value);
-  onOpenChange(false);
+  contextCloseFrom.value = from ?? closeFrom
+  emits('close', contextCloseFrom.value)
+  onOpenChange(false)
 }
 
 function onClose(event: Event, from?: DialogCloseFrom) {
-  emits('click', event);
-  console.log('click', event.defaultPrevented);
-  if (event.defaultPrevented) return true;
-  handleClose(from);
+  emits('click', event)
+  console.log('click', event.defaultPrevented)
+  if (event.defaultPrevented)
+    return true
+  handleClose(from)
 }
 
-const { close } = tvDialog();
-const forwarded = useForwardProps(props);
+const { close } = tvDialog()
+const forwarded = useForwardProps(props)
 </script>
 
 <template>
